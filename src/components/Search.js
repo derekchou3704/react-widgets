728ae76934c893623614d00b02597c0a4d3f53ef
DerekChou <derekchou3704@gmail.com>
2021-06-29 23:11:55 +0800

    Add another useEffect to seperate the bouncedTerm part

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,19 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('programming')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
 
-    console.log(results)
+    // Set for debouncing actions
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000)
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [term])
 
     useEffect(() => {
         const search = async () => {
@@ -15,13 +25,45 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             })
+
             setResults(data.query.search)
         }
+
         search()
-    }, [term])
+    }, [debouncedTerm])
+
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: term
+    //             }
+    //         })
+    //         setResults(data.query.search)
+    //     }
+
+    //     if(term && !results.length) {
+    //         search()
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search()
+    //             }
+    //         }, 1000)
+    
+    //         return () => {
+    //             clearTimeout(timeoutId)
+    //         }
+    //     }
+        
+    // }, [term, results.length])
 
     const renderedResults = results.map((result) => {
         return (
