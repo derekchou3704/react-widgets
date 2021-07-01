257c2db7a8cb2c3702d5405935b03de6532719fc
DerekChou <derekchou3704@gmail.com>
2021-07-01 11:54:58 +0800

    Introducing useRef to dropdown to handle the event bubbling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Accordion from './components/Accordion'
-import Search from './components/Search'
+// import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 // const items = [
 //     {
@@ -17,12 +18,39 @@ import Search from './components/Search'
 //     }
 // ]
 
+const options = [
+    {
+        label: 'Red',
+        value: 'red'
+    },
+    {
+        label: 'Blue',
+        value: 'blue'
+    },
+    {
+        label: 'Green',
+        value: 'green'
+    }
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0])
+    const [showDropdown, setShowDropdown] = useState(true)
+
     return (
         <div>
             <br />
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown ? 
+                <Dropdown 
+                    selected={selected} 
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+                : null
+            }
         </div>
     )
 }
