bc01a639b1028d1bff9201b767853fb28233d9e9
DerekChou <derekchou3704@gmail.com>
2021-07-02 10:24:36 +0800

    Setup a basic Router component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,65 @@
 import React, { useState } from 'react'
-// import Accordion from './components/Accordion'
-// import Search from './components/Search'
-// import Dropdown from './components/Dropdown'
-import Translate from './components/Translate'
+import Accordion from './components/Accordion'
+import Search from './components/Search'
+import Dropdown from './components/Dropdown'
+// import Translate from './components/Translate'
+import Route from './components/Route'
 
-// const items = [
-//     {
-//         title: 'What is react',
-//         content: 'React is a front-end JS framework'
-//     },
-//     {
-//         title: 'Why React?',
-//         content: 'Popular'
-//     },
-//     {
-//         title: 'How do you use React?',
-//         content: 'We use React to '
-//     }
-// ]
+const items = [
+    {
+        title: 'What is react',
+        content: 'React is a front-end JS framework'
+    },
+    {
+        title: 'Why React?',
+        content: 'Popular'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'We use React to '
+    }
+]
 
-// const options = [
-//     {
-//         label: 'Red',
-//         value: 'red'
-//     },
-//     {
-//         label: 'Blue',
-//         value: 'blue'
-//     },
-//     {
-//         label: 'Green',
-//         value: 'green'
-//     }
-// ]
+const options = [
+    {
+        label: 'Red',
+        value: 'red'
+    },
+    {
+        label: 'Blue',
+        value: 'blue'
+    },
+    {
+        label: 'Green',
+        value: 'green'
+    }
+]
+
 
 export default () => {
-    // const [selected, setSelected] = useState(options[0])
+    const [selected, setSelected] = useState(options[0])
     // const [showDropdown, setShowDropdown] = useState(true)
 
     return (
         <div>
-            <br />
-            {/* <Accordion items={items}/> */}
-            {/* <Search /> */}
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    selected={selected} 
+                    onSelectedChange={setSelected}
+                    options={options} 
+                />
+            </Route>
+            {/* <Route path="/translate">
+                <Translate />
+            </Route> */}
+
             {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
             {showDropdown ? 
                 <Dropdown 
@@ -52,7 +69,7 @@ export default () => {
                 />
                 : null
             } */}
-            <Translate />
+            
         </div>
     )
 }
